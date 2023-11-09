6588ab2fee2f732f90f85a65fc848e82cf98b47f
RaaOrorp <fonovaleksandr37@gmail.com>
2023-11-09 14:35:30 +0300

    Изменили game.py

--- Game.py
+++ Game.py
@@ -5,14 +5,18 @@ num_stones = random.randint(4, 30)
 print(f"В кучке {num_stones} камней.")
 
 while num_stones > 0:
-    player_choice = int(input("Сколько камней вы хотите взять (1-3)?: "))
-    while player_choice < 1 or player_choice > 3:
-        player_choice = int(input("Вы можете взять от 1 до 3 камней. Сколько камней вы хотите взять (1-3)? "))
+    try:
+        player_choice = int(input("Сколько камней вы хотите взять (1-3)?: "))
+        while player_choice < 1 or player_choice > 3:
+            player_choice = int(input("Вы можете взять от 1 до 3 камней. Сколько камней вы хотите взять (1-3)? "))
+    except ValueError:
+        print("Введите число от 1 до 3")
+        continue
 
     num_stones -= player_choice
     print(f"В кучке осталось {num_stones} камней.")
 
-    if num_stones == 0:
+    if num_stones <= 0:
         print("Вы выиграли!")
         exit()
 
@@ -21,6 +25,6 @@ while num_stones > 0:
     print(f"Компьютер взял {computer_choice} камней.")
     print(f"В кучке осталось {num_stones} камней.")
 
-    if num_stones == 0:
+    if num_stones <= 0:
         print("Компьютер выиграл!")
         exit()
